5f24dcd7aa966b68167c22f665e83437cb8d58a2
heartzhacker <iit2022050@iiitl.ac.in>
2024-07-06 09:26:52 +0530

    Adding Files

---
+++ frontend/src/pages/AboutUs.jsx
@@ -0,0 +1,25 @@
+
+
+const AboutUs = () => {
+  return (
+    <div className="text-color ms-5 me-5 mr-5 mt-3">
+      <b>
+      Welcome to M25 Canteen , where innovation meets efficiency.
+
+At M25 canteen , we revolutionize canteen management by providing cutting-edge automation solutions tailored to streamline and enhance the canteen experience for institutions, corporate offices, and public facilities. Our mission is to simplify the complexities of canteen operations while ensuring a seamless and convenient dining experience for patrons and administrators alike.
+        <br />
+        <br />
+        With a team of passionate tech enthusiasts and industry experts, we've engineered a comprehensive system that integrates state-of-the-art technology to automate the entire canteen ecosystem. From digital menu displays and cashless transactions to inventory management and analytics, our system is designed to optimize operations, reduce wait times, and provide real-time insights for efficient decision-making.
+
+Driven by a commitment to innovation, reliability, and customer satisfaction, we continuously strive to evolve our system, incorporating the latest advancements to meet the dynamic needs of modern canteens. Our user-friendly interfaces and customizable features ensure adaptability across diverse environments, empowering canteen managers to orchestrate operations effortlessly while offering patrons a convenient, tech-enabled dining experience.
+        <br />
+        <br />
+
+        At M25, we stand at the forefront of the canteen automation revolution, empowering businesses and institutions to embrace the future of food service management. Join us in transforming canteen operations and delivering unparalleled convenience, efficiency, and satisfaction to all stakeholders.
+        
+      </b>
+    </div>
+  );
+};
+
+export default AboutUs;
---
+++ frontend/src/pages/ContactUs.jsx
@@ -0,0 +1,36 @@
+
+const ContactUs = () => {
+  return (
+    <div className="text-color ms-5 me-5 mr-5 mt-3">
+      <b>
+      Thank you for your interest in our M25 ! We're eager to assist you. Below, you'll find various ways to get in touch with us:
+      
+Customer Support
+Do you need help or have questions about our system's features? Our dedicated customer support team is available to assist you. Reach out via email at support@[M25].com or call us at [8764139399] during our support hours, [8:00AM - 8:00PM].
+
+Sales and Partnerships
+For inquiries about sales, partnerships, or to request a demonstration of our canteen automation system, please contact our sales team. Email us at sales@[M25].com or call [8764139399]. We're excited to discuss how our system can meet your needs.
+
+        <br />
+        <br />
+        Address
+If you prefer traditional mail or wish to visit us, our headquarters are located at:
+[M25]
+[IIIT LUCKNOW]
+[LUCKNOW,UTTARPRADESH]
+[INDIA]
+
+Social Media
+Stay updated with our latest news, product updates, and industry insights by following us on social media. Connect with us on Twitter, LinkedIn, Facebook to join the conversation and engage with our community.
+
+Feel free to reach out to us using any of the methods mentioned above. We value your feedback and look forward to assisting you with our innovative canteen automation solutions.
+
+Thank you for choosing M25!
+
+
+      </b>
+    </div>
+  );
+};
+
+export default ContactUs;
